04688a5bf7092e9cf913ad0da1f6b6f9218b1cd9
Yana <yana.rusnak.93@icloud.com>
2024-04-01 18:05:44 +0300

    add .gitignore

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketprice = 15_653;
+        
+        double bonus = ticketprice / 20;
+
+        System.out.println(bonus);
+
+
+    }
+}
+
+
