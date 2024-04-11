e04bb8719a5236379b9dc50708be0d870f8b1c2a
Yana <yana.rusnak.93@icloud.com>
2024-04-11 11:50:04 +0300

    add src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int ticketprice = 15_653;
-        
+
         double bonus = ticketprice / 20;
 
         System.out.println(bonus);
